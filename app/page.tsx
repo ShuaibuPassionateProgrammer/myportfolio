cf74d4eac297a9c2d15cf81ae09474b72f9a5f0f
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-05-28 17:14:51 +0100

    feat: import Footer component in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
