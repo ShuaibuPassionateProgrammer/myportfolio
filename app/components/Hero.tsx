8490268b3d7c00b65bfe5e62599344595177ff62
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-05-28 14:14:09 +0100

    chore: update Hero component structure and ensure framer-motion import is included

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,3 +3,10 @@
 import React, { useState } from "react";
 import { ArrowDownToLine, Download } from "lucide-react";
 import { motion } from "framer-motion";
+
+export default function Hero() {
+
+    return (
+        <section className="text-center space-y-4 pt-10"></section>
+    );
+}
