8ebb9058edcff2678475f86c2579e9c037c455eb
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-05-28 14:20:35 +0100

    chore: update Hero component heading to include name

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -13,7 +13,7 @@ export default function Hero() {
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.6 }}
                 className="text-4xl sm:text-5xl font-heading font-bold">
-                {/*  */}
+                Hi, I'm <span className="text-indigo-600">Ibrahim Shuaibu Isa</span>
             </motion.h1>
         </section>
     );
